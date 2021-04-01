eb1b5cc6a74a79883a8d16a6f7888b33f55a1c61
Shreyas Chaudhari <shreya.chaudhari@sbdinc.com>
2021-04-01 19:06:27 +0530

    Initiated react project with cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello!!</h1>
+      <input type='text' />
+      <button>Add Todo</button>
+      <ul>
+        <li>Take dogs for a walk</li>
+        <li>Who let the dogs out?</li>
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default app;
